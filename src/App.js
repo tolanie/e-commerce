0bb185f8c815d76d66c792b023ac03db88c154b1
tolanie <florenceabiodun2000@yahoo.com>
2021-09-04 22:19:25 +0100

    updating our app with a shop page, collection-component and collection preview component

--- src/App.js
+++ src/App.js
@@ -6,19 +6,15 @@ import './App.css';
 
 import HomePage from './pages/Homepage/homepage.component';
 
-const HatsPage = () =>(
-  <div>
-    <h1>
-      HATS PAGE
-    </h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component';
+
+
 
 function App() {
   return (
     <div>
       <Route exact path='/' component = { HomePage } />
-      <Route path='/hats' component = { HatsPage } />
+      <Route path='/shop' component = { ShopPage } />
     </div>
   );
   
--- src/components/Menu-item/menu-item.component.jsx
+++ src/components/Menu-item/menu-item.component.jsx
@@ -9,7 +9,7 @@ const MenuItem =({title, imageUrl, size, history, linkUrl, match}) => (
     className= {`${size} menu-item`} onClick= {() => history.push(`${match.url}${linkUrl}`)}
     > 
       <div className= 'background-image' style= {{
-         backgroundImage: `url(${imageUrl})`
+         backGroundImage: `url(${imageUrl})`
       }} />
     <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
