0c728d998683d3ef7071f4bba81d720106f9a037
DanLowo <danlowo@gmail.com>
2021-09-05 18:56:31 +0100

    fix background image

--- src/components/Menu-item/menu-item.component.jsx
+++ src/components/Menu-item/menu-item.component.jsx
@@ -9,7 +9,7 @@ const MenuItem =({title, imageUrl, size, history, linkUrl, match}) => (
     className= {`${size} menu-item`} onClick= {() => history.push(`${match.url}${linkUrl}`)}
     > 
       <div className= 'background-image' style= {{
-         backGroundImage: `url(${imageUrl})`
+          backgroundImage: `url(${imageUrl})`
       }} />
     <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
