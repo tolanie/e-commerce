b5e5b7cbe97338923fdc60f85ac21f56a505e8a2
tolanie <florenceabiodun2000@yahoo.com>
2021-09-05 17:52:23 +0100

    where im having problems

--- src/App.js
+++ src/App.js
@@ -8,13 +8,19 @@ import HomePage from './pages/Homepage/homepage.component';
 
 import ShopPage from './pages/shop/shop.component';
 
+import Header from './components/Header/header.component.jsx';
+
+
 
 
 function App() {
   return (
     <div>
+      <header/>
+      <switch>
       <Route exact path='/' component = { HomePage } />
       <Route path='/shop' component = { ShopPage } />
+      </switch>
     </div>
   );
   
